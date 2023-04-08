298c40e21087aaf71529bb181da5f94f50bfd326
Nishant <86074054+WrongMice@users.noreply.github.com>
2023-04-08 23:28:58 +0530

    created jus Ui of artistwiseProduct

--- Front-end/components/NavBar.js
+++ Front-end/components/NavBar.js
@@ -12,10 +12,10 @@ const NavBar = ({ cart }) => {
                 </a></Link>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <Link legacyBehavior href="/"><a className="mr-5 hover:text-gray-900">Home</a></Link>
-                    <Link legacyBehavior href="/about"><a className="mr-5 hover:text-gray-900">About</a></Link>
-                    <Link legacyBehavior href="/products"><a className="mr-5 hover:text-gray-900">Products</a></Link>
-                    <Link legacyBehavior href="/contact"><a className="mr-5 hover:text-gray-900">Contact Us</a></Link>
                     <Link legacyBehavior href="/Artists/artistProfiles"><a className="mr-5 hover:text-gray-900">Artists</a></Link>
+                    <Link legacyBehavior href="/products"><a className="mr-5 hover:text-gray-900">Products</a></Link>
+                    <Link legacyBehavior href="/about"><a className="mr-5 hover:text-gray-900">About</a></Link>
+                    <Link legacyBehavior href="/contact"><a className="mr-5 hover:text-gray-900">Contact Us</a></Link>
                     <Link legacyBehavior href="/checkout"><a className="mr-5 hover:text-gray-900">Cart({cart.length})</a></Link>
                 </nav>
                 <button className=" my-2 text-white bg-indigo-500 border-0 md:py-2 px-4 md:px-2 focus:outline-none hover:bg-indigo-600 rounded text-sm">LogIn</button>
