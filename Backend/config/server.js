cfbffbd2446c249c32e24e35453ac261597200d9
Nishant <86074054+WrongMice@users.noreply.github.com>
2023-04-27 09:28:31 +0530

    deploy strapi on render

--- Backend/config/server.js
+++ Backend/config/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "ecomforartisans.onrender.com"),
+  port: env.int("PORT", 1337),
+  url: env("PUBLIC_URL", "https://ecomforartisans.onrender.com/admin"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
