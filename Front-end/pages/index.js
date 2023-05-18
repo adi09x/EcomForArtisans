f310f0806368fe339b53bdeb5c4b8e3159eb668a
Nishant <86074054+WrongMice@users.noreply.github.com>
2023-05-18 10:36:21 +0530

    installed the plugin for ER Diagram

--- Front-end/pages/index.js
+++ Front-end/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-5 mx-auto">
           <div className="flex flex-col text-center w-full mb-20">
-            <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">BFL COE</h2>
+            {/* <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">BFL COE</h2> */}
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">E-Commerce for Artisians</h1>
             <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven&apos;t heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
           </div>
